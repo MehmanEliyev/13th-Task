63b58029b8e41511424d2e7ae85a4da37c9499a1
Mehman Aliyev <mehman.va@gmail.com>
2023-10-09 19:50:06 +0400

    Add Reset button

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const addNumber = document.querySelector(".addNumber");
 const plus = document.querySelector(".plus");
 const increment = document.querySelector(".increment");
 const decrement = document.querySelector(".decrement");
+const reset = document.querySelector(".reset");
 const calc = document.querySelector(".result");
 
 let count = 0;
@@ -26,6 +27,12 @@ increment.addEventListener("click", ()=>{
 decrement.addEventListener("click", ()=>{
     
     result--;
+    calc.innerHTML = "Result :   " + result;
+    count = result;
+})
+
+reset.addEventListener("click", ()=>{
+    result = result - result; 
     calc.innerHTML = "Result :   " + result;
     count = result;
 })
