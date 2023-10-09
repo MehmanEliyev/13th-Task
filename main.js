f93af5c5e0fc657187af876a578f46ae59e40e8c
Mehman Aliyev <mehman.va@gmail.com>
2023-10-09 19:43:57 +0400

    Solved Counter Task

---
+++ main.js
@@ -0,0 +1,32 @@
+const addNumber = document.querySelector(".addNumber");
+const plus = document.querySelector(".plus");
+const increment = document.querySelector(".increment");
+const decrement = document.querySelector(".decrement");
+const calc = document.querySelector(".result");
+
+let count = 0;
+let result = 0;
+
+
+plus.addEventListener("click" , ()=>{
+
+    result = count + (+addNumber.value);
+    calc.innerHTML = "Result :   " + result;
+    count = result;
+    addNumber.value = "";
+})
+
+increment.addEventListener("click", ()=>{
+
+    result++;
+    calc.innerHTML ="Result :   " + result;
+    count = result;
+})
+
+decrement.addEventListener("click", ()=>{
+    
+    result--;
+    calc.innerHTML = "Result :   " + result;
+    count = result;
+})
+
